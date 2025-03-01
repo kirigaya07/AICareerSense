334f58ebb6e9bc1c0cee9c193434364ae5b85d3d
kirigaya07 <crimsomemoon7@gmail.com>
2025-03-02 00:49:38 +0530

    Hero section added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+import HeroSection from "@/components/hero";
+
 export default function Home() {
   return (
     <>
       <div className="grid-background"></div>
+
+      {/*  Hero Section */}
+      <HeroSection />
     </>
   );
 }
