1cb6c44ab345787c6039ebc481673ca92e881b51
kirigaya07 <crimsomemoon7@gmail.com>
2025-03-06 15:54:51 +0530

    Add IndustryInsight and update OnboardingPage to handle user onboarding status

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
+
+const IndustryInsight = async () => {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  return <div>IndustryInsight</div>;
+};
+
+export default IndustryInsight;
--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import OnboardingForm from "./_components/onboarding-form";
 import { industries } from "@/data/industries";
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
 
-const OnboardingPage = () => {
+const OnboardingPage = async () => {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
   return (
     <main>
       <OnboardingForm industries={industries} />
