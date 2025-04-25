27a04baac6ad6d3ebe5e13ac855280ed887c8dc0
Anmol Chandrakar <crimsomemoon7@gmail.com>
2025-04-25 17:34:06 +0530

    Update HeroSection image source to use Cloudinary URL and add remote pattern for Cloudinary in next.config

--- components/hero.jsx
+++ components/hero.jsx
@@ -55,7 +55,7 @@ const HeroSection = () => {
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner.jpeg"
+              src="https://res.cloudinary.com/ddq4io1nj/image/upload/f_auto,q_auto/khvbnbo2gnribhbegnqv"
               width={1280}
               height={720}
               alt="Dashboard Preview"
